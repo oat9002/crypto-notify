edf09a9a3f7870657056551b8ef4ea4d43d6b807
oat9002 <oat9002@hotmail.com>
2021-06-01 01:35:52 +0700

    :recycle:

--- src/main/scala/processors/Executor.scala
+++ src/main/scala/processors/Executor.scala
@@ -18,6 +18,6 @@ class ExecutorImpl {
   val quartzService: QuartzService[ExecuteTask] = wire[QuartzServiceImpl[ExecuteTask]]
 
   def execute(): Unit = {
-    quartzService.schedule(SchedulerName.Every12And18Hours, system, ExecuteTask())
+    quartzService.schedule(SchedulerName.Every10And18Hours, system, ExecuteTask())
   }
 }
