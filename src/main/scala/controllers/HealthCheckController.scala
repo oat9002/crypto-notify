c55c3e6fe3872297c37a01c49bda6afccd86a9cb
oat9002 <oat9002@hotmail.com>
2021-08-09 00:30:02 +0700

    :sparkles: HealthCheckController

---
+++ src/main/scala/controllers/HealthCheckController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Route
+import models.mackerel.MackerelRequest
+import services.MackerelService
+
+import scala.concurrent.ExecutionContext
+
+class HealthCheckController(mackerelService: MackerelService)(implicit system: ActorSystem[Nothing], context: ExecutionContext) {
+  val route: Route = {
+    path("healthCheck") {
+      get {
+        mackerelService.sendMeasurement(List(MackerelRequest("healthCheck", 1)))
+        complete(HttpEntity(ContentTypes.`application/json`, "alive"))
+      }
+    }
+  }
+}
