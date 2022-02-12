99eb552f75b2ef9f5c9fda44bc417b9d49d86256
oat9002 <oat9002@hotmail.com>
2022-02-13 00:08:20 +0700

    :construction:

--- src/main/scala/services/BinanceService.scala
+++ src/main/scala/services/BinanceService.scala
@@ -3,12 +3,13 @@ package services
 import akka.actor.typed.ActorSystem
 import com.typesafe.scalalogging.LazyLogging
 import commons.{CommonUtil, Configuration, HmacAlgorithm, HttpClient}
-import models.binance.Saving
+import models.binance.{Account, Saving}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 trait BinanceService {
   def getSaving: Future[Option[Saving]]
+  def getAccountDetail: Future[Option[Account]]
 }
 
 class BinanceServiceImpl(configuration: Configuration, httpClient: HttpClient)(implicit system: ActorSystem[Nothing], context: ExecutionContext) extends BinanceService with LazyLogging {
@@ -22,6 +23,21 @@ class BinanceServiceImpl(configuration: Configuration, httpClient: HttpClient)(i
     response map {
       case Left(err) =>
         logger.error(s"getSaving failed: $err")
+        None
+      case Right(saving) => Some(saving)
+    }
+  }
+
+  override def getAccountDetail: Future[Option[Account]] = {
+    val timeStamp = System.currentTimeMillis()
+    val queryString = s"timestamp=$timeStamp"
+    val signature = CommonUtil.generateHMAC(queryString, configuration.binanceConfig.secretKey, HmacAlgorithm.HmacSHA256)
+    val url = s"${configuration.binanceConfig.url}/sapi/v1/capital/config/getall?$queryString&signature=$signature"
+    val response = httpClient.get[Any, Account](url, None, Map("X-MBX-APIKEY" -> configuration.binanceConfig.apiKey))
+
+    response map {
+      case Left(err) =>
+        logger.error(s"getAccountDetail failed: $err")
         None
       case Right(saving) => Some(saving)
     }
