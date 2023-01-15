2bb59795ff835b98f3f978c6ffba613d53ac2c55
oat9002 <oat9002@hotmail.com>
2023-01-15 15:53:03 +0700

    :package: add logback

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,7 @@ object Dependencies {
   val macWire = "2.5.8"
   val scalaLogging = "3.9.4"
   val slf4j = "2.0.3"
+  val logback = "1.4.5"
   val scalaTest = "3.2.10"
   val sttp = "3.8.2"
   val web3j = "4.9.1"
@@ -32,6 +33,7 @@ object Dependencies {
     "org.slf4j" % "slf4j-simple" % slf4j,
     "com.typesafe.scala-logging" %% "scala-logging" % scalaLogging excludeAll
       ExclusionRule(organization = "org.slf4j"),
+    "ch.qos.logback" % "logback-classic" % logback,
     "com.softwaremill.sttp.client3" %% "core" % sttp cross CrossVersion.for3Use2_13,
     "com.softwaremill.sttp.client3" % "akka-http-backend" % sttp cross CrossVersion.for3Use2_13,
     "com.softwaremill.sttp.client3" %% "circe" % sttp cross CrossVersion.for3Use2_13 excludeAll
